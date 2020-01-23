07984d556ff5a3c8794ca15011216b47ad7ae72a
Barinov, Mikhail <mikhail.barinov.consultant@nielsen.com>
2020-01-23 17:22:04 +0300

    additional tasks done (localization, userDefaults, settingsScreen (changing randomize range), checkCountLabel)

---
+++ practice/SettingsVC.swift
@@ -0,0 +1,59 @@
+//
+//  SettingsVC.swift
+//  practice
+//
+//  Created by Mikhail Barinov on 23.01.2020.
+//  Copyright © 2020 Mikhail Barinov. All rights reserved.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var minValueLabel: UILabel!
+    @IBOutlet weak var minValueTextField: UITextField!
+    @IBOutlet weak var maxValueLabel: UILabel!
+    @IBOutlet weak var maxValueTextField: UITextField!
+    
+    @IBAction func backButtonAction(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        defaults.set(Int(minValueTextField.text!), forKey: "MIN_VALUE")
+        defaults.set(Int(minValueTextField.text!), forKey: "MIN_VALUE")
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        backButton.setTitle("BACK", for: .normal)
+        minValueLabel.text = "MIN_VALUE_TITLE_MESSAGE"
+        maxValueLabel.text = "MAX_VALUE_TITLE_MESSAGE"
+        minValueTextField.text = String(UserDefaults.standard.integer(forKey: "MIN_VALUE"))
+        maxValueTextField.text = String(UserDefaults.standard.integer(forKey: "MAX_VALUE"))
+    }
+    
+    @IBAction func userNumberChangedAction(_ sender: Any) {
+        if sender is UITextField {
+            let textField = sender as! UITextField
+            let aSet = NSCharacterSet(charactersIn:"0123456789").inverted
+            if let userText = textField.text {
+                let compSepByCharInSet = userText.components(separatedBy: aSet)
+                let numberFiltered = compSepByCharInSet.joined(separator: "")
+                if (numberFiltered != "") {
+                    textField.text = numberFiltered
+                } else {
+                    switch textField {
+                    case minValueTextField:
+                        textField.text = String(UserDefaults.standard.integer(forKey: "MIN_VALUE"))
+                    case maxValueTextField:
+                        textField.text = String(UserDefaults.standard.integer(forKey: "MAX_VALUE"))
+                    default:
+                        break
+                    }
+                }
+            }
+        }
+    }
+}
